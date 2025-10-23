01bca6898c4de2f27246c50e87563f9d972eafbd
Abrar242002 <abrar242002@gmail.com>
2025-10-23 18:09:45 +0600

    practice for if else (error fixed)

--- script.js
+++ script.js
@@ -142,12 +142,12 @@ console.log(canVote); // "Yes, can vote"
 
 // if else statement
 
-if (condition1) {
+/* if (condition1) {
   // code to execute if condition1 is true
 } else if (condition2) {
   // code to execute if the condition1 is false and condition2 is true
 } else {
   // code to execute if the condition1 is false and condition2 is false
 }
-
+*/
 // ////////////Pracitse More/////////
