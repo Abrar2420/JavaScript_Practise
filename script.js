f46f0c48662ed22a6e54c19467269c3e205b9d6c
Abrar242002 <abrar242002@gmail.com>
2025-10-27 16:09:13 +0600

    practice for ternary operetor with conditions

---
+++ script.js
@@ -0,0 +1,34 @@
+// ternary operator
+// light/dark mode switcher with ternary (? :) operator
+
+// let mode = prompt("enter 'l' for light mode or 'd' for dark mode:");
+
+// mode == "l" //condition 1
+//   ? alert("Light mode on.") // runs if condition 1 is true
+//   : mode == "d" // condition 2
+//   ? alert("Dark mode on.") // runs if condition 2 is true
+//   : alert("Invalid input"); // runs if both conditon 1 & 2 are flase, as that will make it true by default.
+
+// discount eligibility tester
+
+// let discount = +prompt("Enter purchase amount:");
+
+// discount >= 3000
+//   ? alert(
+//       `Congrats you ar eligible for 30% discount, you discounted amount is ${
+//         (discount / 100) * 30
+//       }TK`
+//     )
+//   : discount >= 2000
+//   ? alert(
+//       `Congrats you are eligible for 20% discount, you discounted amount is ${
+//         (discount / 100) * 20
+//       }TK`
+//     )
+//   : discount >= 1000
+//   ? alert(
+//       `Congrats you are eligible for 10% discount, you discounted amount is ${
+//         (discount / 100) * 10
+//       }TK`
+//     )
+//   : alert(`You are not eligible for any discount`);
