637fda93a8f23bf23f116fe1f9f696fe41babcd5
Abrar242002 <abrar242002@gmail.com>
2025-10-30 14:29:29 +0600

    practice for function

--- functions.js
+++ functions.js
@@ -1,8 +1,43 @@
-// JavaScrit Functions
+let test = "This is a test variable";
 
-// Function to add two numbers
-function add(a, b) {
-  return a + b;
+let priceInput = +prompt("Enter the Price:");
+let quantityInput = +prompt("Enter the Quantity:");
+
+function totalPrice(price, quantity) {
+  return price * quantity;
 }
 
-console.log(add(5, 3)); // Output: 8
+function addVat(price, quantity, vatPercentage) {
+  let basePrice = totalPrice(price, quantity);
+  let vat = (basePrice / 100) * vatPercentage;
+  return basePrice + vat;
+}
+
+function addDiscount(price, quantity, vatPercentage, discountPercentage) {
+  let priceWithVat = addVat(price, quantity, vatPercentage);
+  let discount = (priceWithVat / 100) * discountPercentage;
+  return priceWithVat - discount;
+}
+
+function showBill(price, quantity, vatPercentage, discountPercentage) {
+  let total = totalPrice(price, quantity);
+  let withVat = addVat(price, quantity, vatPercentage);
+  let withDiscount = addDiscount(
+    price,
+    quantity,
+    vatPercentage,
+    discountPercentage
+  );
+  console.log(`
+              ---------------- Customer Invoice ----------------
+                    total price : ${total} TK
+                    vat added   : 15 %
+                    -----------------------------------
+                    total after vat : ${withVat} TK
+                    discount given  : 20 %
+                    -----------------------------------
+                    sub total       : ${withDiscount} TK
+    `);
+}
+
+showBill(priceInput, quantityInput, 15, 20);
