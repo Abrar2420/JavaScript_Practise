88125646fc723d73e7fba22f74f7316b41059222
Abrar242002 <abrar242002@gmail.com>
2025-11-13 20:03:32 +0600

    DOM Nav Practice some comments added

--- DOM/nav.js
+++ DOM/nav.js
@@ -1,8 +1,17 @@
 import { createElement } from "./elements.js";
-import { navStyle, navAttr } from "./styles.js";
+import { navStyle, navAttr, containerStyle, containerAttr } from "./styles.js";
 // creating github navbar JS
 
 const root = document.querySelector(".root");
-// console.log(root);
 
-console.log(createElement("nav", root, navAttr, navStyle));
+//* the createELement function will create an element,
+//* and add its styling and set its attributes,
+//* and then also append the element to the path provided
+//* it takes four parameters which are:
+//*  --> 1. the name of the element (tag) we want to create
+//*  --> 2. the path (location in document), where we want to append the element
+//*  --> 3. the attributes (takes an object) we want to set for the element
+//*  --> 4. the styles (takes an object) we want to set for the element
+//* and then it returns the new element
+const nav = createElement("nav", root, navAttr, navStyle);
+const container = createElement("div", nav, containerAttr, containerStyle);
--- DOM/styles.js
+++ DOM/styles.js
@@ -1,7 +1,14 @@
 // making style as objects
 export const navStyle = {
   "background-color": "#25292e",
+  height: "80px",
+  color: "#c8c9ca",
+};
+export const containerStyle = {
+  "background-color": "#1266ceff",
   height: "60px",
+  width: "90%",
+  margin: "auto",
   color: "#c8c9ca",
 };
 
@@ -9,4 +16,8 @@ export const navStyle = {
 export const navAttr = {
   class: "nav",
   id: "navbar",
+};
+export const containerAttr = {
+  class: "container",
+  id: "",
 };
