d2b9c6d734ffea8061f2c0a4e7935bcf7fd7ebe2
Abrar242002 <abrar242002@gmail.com>
2025-12-14 17:07:58 +0600

    more practise of JS function closures

--- closures.js
+++ closures.js
@@ -30,3 +30,33 @@ addCount();
 addCount();
 addCount();
 addCount();
+
+const bank = () => {
+  let balance = 0;
+  return {
+    deposit: (amount = 0) => {
+      console.log("previous balance", balance);
+      balance += amount;
+      console.log("deposit amount:", amount);
+      console.log("current balance:", balance);
+    },
+    withdraw: (amount) => {
+      if (amount > balance) {
+        console.log("insufficient amount");
+      } else {
+        console.log("previous balance", balance);
+        balance -= amount;
+        console.log("deposit amount:", amount);
+        console.log("current balance:", balance);
+      }
+    },
+    checkBalance: () => {
+      console.log(`current balance: ${balance}`);
+    },
+  };
+};
+
+const myBank = bank();
+myBank.deposit(500);
+myBank.withdraw(100);
+myBank.checkBalance();
